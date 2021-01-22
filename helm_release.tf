781e00c9988bae0832f645da9b1a90b3f4fc0260
Rachel Sharp <rachel@hashicorp.com>
2021-01-22 14:58:24 -0500

    new auth process helm>kube

--- helm_release.tf
+++ helm_release.tf
@@ -2,12 +2,13 @@ provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
   }
 }
-
-
-
 resource "helm_release" "kubewatch" {
   name       = "kubewatch"
   repository = "https://charts.bitnami.com/bitnami"
