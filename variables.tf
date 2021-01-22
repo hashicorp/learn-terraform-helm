627c42852d94f779e39b29f38642f03acf843dc8
Rachel Sharp <rachel@hashicorp.com>
2021-01-21 19:49:18 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,7 @@
-variable "application_name"{}
+variable "application_name"{
+  type = string
+  default = "terramino"
+}
 variable "slack_app_token" {
   type = string
   description = "Slack App Token"
