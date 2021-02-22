15c1a5907a61bde3f52339c631a23154c9068745
Rachel Sharp <rachel@hashicorp.com>
2021-02-22 18:44:07 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -19,7 +19,6 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
-  load_config_file       = "false"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
